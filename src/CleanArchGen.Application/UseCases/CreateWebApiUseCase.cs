7bf2ce599904726be560af1e5e9ba91977b78d4b
cristian <cristianboiam@hotmail.com>
2020-04-08 21:36:23 -0300

    Generate src folder on CreateWebApiUseCase

--- src/CleanArchGen.Application/UseCases/CreateWebApiUseCase.cs
+++ src/CleanArchGen.Application/UseCases/CreateWebApiUseCase.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
 using CleanArchGen.Domain.DefaultLayers;
 using CleanArchGen.Domain.Interfaces;
 
@@ -8,36 +6,37 @@ namespace CleanArchGen.Application.UseCases
 {
     public class CreateWebApiUseCase
     {
-        private IEnumerable<IDefaultLayer> _defaultLayers;
-
+        private readonly IEnumerable<IDefaultLayer> _defaultLayers;
         private readonly ISolution _solution;
+        private readonly IDirectoryCreator _directoryCreator;
 
-        public CreateWebApiUseCase(IEnumerable<IDefaultLayer> defaultLayers, ISolution solution)
+        public CreateWebApiUseCase(IEnumerable<IDefaultLayer> defaultLayers, ISolution solution, IDirectoryCreator directoryCreator)
         {
             _defaultLayers = defaultLayers;
             _solution = solution;
+            _directoryCreator = directoryCreator;
         }
 
         public void Create(string path, string name)
         {
             _solution.Create(path, name);
-            
-            // Create src folder
+
+            _directoryCreator.CreateAndSetAsCurrentPath("src");
 
             // Create Domain, Application, Infra layers
             // foreach (var layer in _defaultLayers)
             // {
             //     layer.Create(project);
             // }
-            
-            
+
+
             // Create Web Api
-            
+
             // Create tests folder
             // Create UnitTests project
             // Create IntegrationTests project
             // Create FuncionalTests classlib
-            
+
         }
     }
 }
--- tests/CleanArchGen.UnitTests/Application/UseCases/CreateWebApiUseCaseTest.cs
+++ tests/CleanArchGen.UnitTests/Application/UseCases/CreateWebApiUseCaseTest.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using CleanArchGen.Application.UseCases;
+using CleanArchGen.Domain.DefaultLayers;
+using CleanArchGen.Domain.Interfaces;
+using Moq;
 using Xunit;
 
 namespace CleanArchGen.UnitTests
@@ -5,9 +10,23 @@ namespace CleanArchGen.UnitTests
     public class CreateWebApiUseCaseTest
     {
         [Fact]
+        public void Execute_ShouldCreateSrcFolder()
+        {
+            var defaultLayers = new Mock<IEnumerable<IDefaultLayer>>();
+            var solution = new Mock<ISolution>();
+            var directoryCreator = new Mock<IDirectoryCreator>();
+
+            var useCase = new CreateWebApiUseCase(defaultLayers.Object, solution.Object, directoryCreator.Object);
+
+            useCase.Create("test", "TestProject");
+
+            directoryCreator.Verify(x => x.CreateAndSetAsCurrentPath("src"), Times.Once);
+        }
+
+        [Fact]
         public void Execute_ShouldCreateWebApiProject()
         {
-            
+
         }
     }
 }
