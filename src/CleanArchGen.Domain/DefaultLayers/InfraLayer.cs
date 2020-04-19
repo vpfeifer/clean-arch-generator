1acc613eb7ba786994c25fd33122301197ccc47d
cristian <cristianboiam@hotmail.com>
2020-04-19 14:05:26 -0300

    Create Infra layer

---
+++ src/CleanArchGen.Domain/DefaultLayers/InfraLayer.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchGen.Domain.Interfaces;
+
+namespace CleanArchGen.Domain.DefaultLayers
+{
+    public class InfraLayer : DefaultLayer
+    {
+        public InfraLayer(IDotNetClient dotnet) : base(dotnet) { }
+
+        protected override string GetLayerName() => "Infra";
+    }
+}
---
+++ tests/CleanArchGen.UnitTests/Domain/DefaultLayers/InfraLayerTest.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchGen.Domain.DefaultLayers;
+using CleanArchGen.Domain.Interfaces;
+using Moq;
+using Xunit;
+
+namespace CleanArchGen.UnitTests.Domain.DefaultLayers
+{
+    public class InfraLayerTest
+    {
+        [Fact]
+        public void Create_ShouldCreateProjectWithCorrectNameInSrcFolder()
+        {
+            var dotnetCliMock = new Mock<IDotNetClient>();
+
+            var domainLayer = new InfraLayer(dotnetCliMock.Object);
+
+            var testFolder = "test";
+            var projectName = "FooProject";
+
+            var expectedFolder = $"{testFolder}/src";
+            var expectedName = $"{projectName}.Application";
+
+            domainLayer.Create(testFolder, projectName);
+
+            dotnetCliMock.Verify(x => x.CreateClassLibProject(expectedFolder, expectedName), Times.Once);
+        }
+    }
+}
